fbceaf9e3b443c722a217cafa1a5e682b5ce5ed7
Ayokanmi Adeyemi <101461930+Sovrano28@users.noreply.github.com>
2023-02-10 20:02:55 +0100

    started the work finally
    ...with a bang .lol!

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,13 @@ module.exports = {
   darkMode: 'class',
   
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        WATeal: 'rgb(18, 140, 126)',
+        WADarkTeal: '#1F2C34',
+        WALightGreen: 'rgb(37, 211, 102)'
+      }
+    },
   },
   plugins: [],
 }
