c91cd1f808f4922543d1f8ac12ec2b83b75fac0b
Ayokanmi Adeyemi <101461930+Sovrano28@users.noreply.github.com>
2023-02-15 19:10:24 +0100

    woah!!
    finally was able to use dropdown on that option pane, I learnt about focus(),  blur(), tabindex, animationend event, not-asyn-javascript e.t.c. also added some tailwind default animation keyframes.

--- public/script.js
+++ public/script.js
@@ -1,4 +1,6 @@
 const themeBtn = document.getElementById('themeSwitch');
+const settingBtn = document.getElementById('setting-btn');
+const appOptions = document.getElementById('settings');
 
 themeBtn.addEventListener('click', ()=> {
 
@@ -10,4 +12,26 @@ themeBtn.addEventListener('click', ()=> {
     document.documentElement.classList.add('dark');
     localStorage.setItem('theme', 'dark');
   }
+});
+
+settingBtn.addEventListener('click', ()=> {
+  appOptions.classList.toggle('hidden');
+  appOptions.classList.add('animate__slideInDown');
+
+  appOptions.addEventListener('animationend', ()=> {
+    appOptions.classList.remove('animate__slideInDown');
+  })
+  
+  appOptions.classList.toggle('show');
+  appOptions.focus({focusVisible: false});
+})
+
+appOptions.addEventListener('blur', ()=> {
+  appOptions.classList.add('animate__fadeOut');
+
+  appOptions.addEventListener('animationend', ()=> {
+    appOptions.classList.remove('animate__fadeOut');
+    appOptions.classList.remove('show');
+    appOptions.classList.add('hidden');
+  }, {once: true})
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,10 +13,32 @@ module.exports = {
         WADarkTeal: '#1F2C34',
         WALightGreen: 'rgb(37, 211, 102)'
       },
+      
       fontFamily: {
         roboto: ['Roboto', 'sans-serif']
+      },
+
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-3deg)' },
+          '50%': { transform: 'rotate(3deg)' },
+        },
+        
+        dropdown: {
+          '0%': {top: '-280px'},
+          // '50%': {top: '-140px'},
+          '100%': {top: '0px'}
+        }
+      },
+
+      animation: {
+        wiggle: 'wiggle 1s ease-in-out infinite',
+        dropdown: 'dropdown 0.5s ease-out'
       }
-    },
+    }
   },
-  plugins: [],
+
+  plugins: [
+    require("tailwindcss-animate")
+  ],
 }
