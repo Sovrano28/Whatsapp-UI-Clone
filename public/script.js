89bd1cc266b73f613ed558264c23b90efbc4175f
Ayokanmi Adeyemi <101461930+Sovrano28@users.noreply.github.com>
2023-03-06 21:51:49 +0100

    finally!!!!!!!!!!!
    I made use of createElement() and append() methods. So HTML2JS was very helpful for converting html to js that way. so the most important thing was that I moved those element that I will add event listener to from a local scope to a global scope. I also needed to manually add some classes that those element added by the main event created but are not already owned by classes originally in the dom cos tailwind only add classes only in the dom. I don't know if insertAdjacentHTML or inner HTML would have worked with the whole declaring as a global var stuffs

--- public/script.js
+++ public/script.js
@@ -36,70 +36,170 @@ if (settingBtn != null) {
       appOptions.classList.add('hidden');
     }, {once: true})
   });
-}
+};
 
 const rightColumn = document.getElementById("column-2");
 const myChats = document.getElementsByClassName('chats');
+const rightColumnContent = document.getElementById('column-2-default-inner');
+
+var e_2 = document.createElement("div");
+e_2.setAttribute("id", "chat-settings");
+e_2.setAttribute("class", "animate__animated animate__faster absolute top-0 right-0 w-2/5 z-10 bg-white dark:bg-WADarkGreen text-black dark:text-white shadow-md focus:outline-none");
+e_2.setAttribute("tabindex", "-1");
+
+var e_22 = document.createElement("img");
+e_22.setAttribute("class", "w-6 dual");
+e_22.setAttribute("id", "chat-setting-btn");
+e_22.setAttribute("src", "https://img.icons8.com/external-glyph-silhouettes-icons-papa-vector/100/null/external-Menu-interface-glyph-silhouettes-icons-papa-vector-3.png");
+e_22.setAttribute("alt", "kebab");
 
 if (myChats != null) {
+
   for (var i = 0; i < myChats.length; i++) {
 
     let clickedChat = myChats[i];
     let friendName = clickedChat.getAttribute('friend-name');
     let imageType = clickedChat.getAttribute('img-type');
 
-    clickedChat.addEventListener('click', ()=> {
-      chatPage = `
-      <div class="w-full relative">
-       <div id="chat-settings" class="animate__animated animate__faster absolute hidden top-0 right-0 w-3/5 z-10 bg-white dark:bg-WADarkGreen text-black dark:text-white shadow-md focus:outline-none" tabindex="-1">
-         <ul>
-            <li class="options">View contact</li>
-            <li class="options">Media, links and docs</li>
-            <li class="options">Search</li>
-            <li class="options">Mute notification</li>
-            <li class="options">Disappearing messages</li>
-            <li class="options">Wallpaper</li>
-            <li class="options">More</li>
-         </ul>
-       </div>
-      </div>
-      <header class="flex flex-col w-full bg-WATeal dark:bg-WADarkTeal text-white dark:text-gray-400 p-3">
-      <div class="flex flex-row items-center">
-          <div class="flex">
-            <div class="flex-center">
-              <img class="w-9 h-9 object-cover rounded-full" src="public/imgs/friends/${friendName}.${imageType}" alt="friend">
-            </div>
-            <div class="flex flex-col px-3 py-1">
-              <div class="text-white font-bold text-sm">My Barber</div>
-              <div class="text-white font-normal text-xs">Online</div>
-            </div>
-          </div>
-          <div class="ml-auto flex gap-4">
-            <img class="w-6 dual" src="https://img.icons8.com/android/96/null/video-call.png" alt="video call"/>
-            <img class="w-6 dual" src="https://img.icons8.com/material-sharp/96/000000/phone.png" alt="phone"/>
-            <img class="w-6 dual" id="chat-setting-btn" src="https://img.icons8.com/external-glyph-silhouettes-icons-papa-vector/100/null/external-Menu-interface-glyph-silhouettes-icons-papa-vector-3.png" alt="kebab">
-          </div>
-        </div>
-      </header>
-      <section class="chat-page">
-        <div class="relative z-[1]">
-          <div class="text-[8px] text-center mx-auto my-2 p-1 w-12 text-black dark:text-gray-400 bg-white dark:bg-WADarkTeal rounded-xl shadow-md">Today</div>
-          <div class="text-[8px] text-center mx-auto my-2 p-2 w-80 text-black dark:text-gray-400 bg-WALightYellow dark:bg-WADarkTeal rounded-xl shadow-md">
-            <img class="inline-flex w-2 mb-[2px] mr-[1px]" src="public/imgs/lock.svg" alt="lock">
-            Messages and calls are end-to-end encrypted. No one outside of this chat, not even WhatsApp, can read or listen to them. Tap to learn more
-          </div>
-        </div>
-      </section>`;
-    
-      rightColumn.innerHTML = chatPage;
+    clickedChat.addEventListener('click', function createNode() {
+      console.log('clicked a chat');
+      rightColumnContent.remove()
+
+      var container = document.createDocumentFragment();
+      var e_0 = document.createElement("div");
+      var e_1 = document.createElement("div");
+      e_1.setAttribute("class", "w-full relative");
+      // e_2 modifications former position
+      var e_3 = document.createElement("ul");
+      var e_4 = document.createElement("li");
+      e_4.setAttribute("class", "options");
+      e_4.appendChild(document.createTextNode("View contact"));
+      e_3.appendChild(e_4);
+      var e_5 = document.createElement("li");
+      e_5.setAttribute("class", "options");
+      e_5.appendChild(document.createTextNode("Media, links and docs"));
+      e_3.appendChild(e_5);
+      var e_6 = document.createElement("li");
+      e_6.setAttribute("class", "options");
+      e_6.appendChild(document.createTextNode("Search"));
+      e_3.appendChild(e_6);
+      var e_7 = document.createElement("li");
+      e_7.setAttribute("class", "options");
+      e_7.appendChild(document.createTextNode("Mute notification"));
+      e_3.appendChild(e_7);
+      var e_8 = document.createElement("li");
+      e_8.setAttribute("class", "options");
+      e_8.appendChild(document.createTextNode("Disappearing messages"));
+      e_3.appendChild(e_8);
+      var e_9 = document.createElement("li");
+      e_9.setAttribute("class", "options");
+      e_9.appendChild(document.createTextNode("Wallpaper"));
+      e_3.appendChild(e_9);
+      var e_10 = document.createElement("li");
+      e_10.setAttribute("class", "options");
+      e_10.appendChild(document.createTextNode("More"));
+      e_3.appendChild(e_10);
+      e_2.appendChild(e_3);
+      e_1.appendChild(e_2);
+      e_0.appendChild(e_1);
+      var e_11 = document.createElement("header");
+      e_11.setAttribute("class", "flex flex-col w-full bg-WATeal dark:bg-WADarkTeal text-white dark:text-gray-400 p-3");
+      var e_12 = document.createElement("div");
+      e_12.setAttribute("class", "flex flex-row items-center");
+      var e_13 = document.createElement("div");
+      e_13.setAttribute("class", "flex");
+      var e_14 = document.createElement("div");
+      e_14.setAttribute("class", "flex-center");
+      var e_15 = document.createElement("img");
+      e_15.setAttribute("class", "w-9 h-9 object-cover rounded-full");
+      e_15.setAttribute("src", `public/imgs/friends/${friendName}.${imageType}`);
+      e_15.setAttribute("alt", "friend");
+      e_14.appendChild(e_15);
+      e_13.appendChild(e_14);
+      var e_16 = document.createElement("div");
+      e_16.setAttribute("class", "flex flex-col px-3 py-1");
+      var e_17 = document.createElement("div");
+      e_17.setAttribute("class", "text-white font-bold text-sm");
+      e_17.appendChild(document.createTextNode("My Barber"));
+      e_16.appendChild(e_17);
+      var e_18 = document.createElement("div");
+      e_18.setAttribute("class", "text-white font-normal text-xs");
+      e_18.appendChild(document.createTextNode("Online"));
+      e_16.appendChild(e_18);
+      e_13.appendChild(e_16);
+      e_12.appendChild(e_13);
+      var e_19 = document.createElement("div");
+      e_19.setAttribute("class", "ml-auto flex gap-4");
+      var e_20 = document.createElement("img");
+      e_20.setAttribute("class", "w-6 dual");
+      e_20.setAttribute("src", "https://img.icons8.com/android/96/null/video-call.png");
+      e_20.setAttribute("alt", "video call");
+      e_19.appendChild(e_20);
+      var e_21 = document.createElement("img");
+      e_21.setAttribute("class", "w-6 dual");
+      e_21.setAttribute("src", "https://img.icons8.com/material-sharp/96/000000/phone.png");
+      e_21.setAttribute("alt", "phone");
+      e_19.appendChild(e_21);
+      // e_22 modifications former position
+      e_19.appendChild(e_22);
+      e_12.appendChild(e_19);
+      e_11.appendChild(e_12);
+      e_0.appendChild(e_11);
+      var e_23 = document.createElement("section");
+      e_23.setAttribute("class", "chat-page");
+      var e_24 = document.createElement("div");
+      e_24.setAttribute("class", "relative z-[1]");
+      var e_25 = document.createElement("div");
+      e_25.setAttribute("class", "text-[8px] text-center mx-auto my-2 p-1 w-12 text-black dark:text-gray-400 bg-white dark:bg-WADarkTeal rounded-xl shadow-md");
+      e_25.appendChild(document.createTextNode("Today"));
+      e_24.appendChild(e_25);
+      var e_26 = document.createElement("div");
+      e_26.setAttribute("class", "text-[8px] text-center mx-auto my-2 p-2 w-80 text-black dark:text-gray-400 bg-WALightYellow dark:bg-WADarkTeal rounded-xl shadow-md");
+      var e_27 = document.createElement("img");
+      e_27.setAttribute("class", "inline-flex w-2 mb-[2px] mr-[1px]");
+      e_27.setAttribute("src", "public/imgs/lock.svg");
+      e_27.setAttribute("alt", "lock");
+      e_26.appendChild(e_27);
+      e_26.appendChild(document.createTextNode("\nMessages and calls are end-to-end encrypted. No one outside of this chat, not even WhatsApp, can read or listen to them. Tap to learn more\n"));
+      e_24.appendChild(e_26);
+      e_23.appendChild(e_24);
+      e_0.appendChild(e_23);
+      container.appendChild(e_0);
+
+      rightColumn.append(e_1, e_11, e_23);
     })
   }
 };
 
+// event listeners for chat-option btn and chat-option at md-and-above screens.
+e_22.addEventListener('click', ()=> {
+  e_2.classList.toggle('hidden');
+  e_2.classList.add('animate__slideInDown');
+
+  e_2.addEventListener('animationend', ()=> {
+    e_2.classList.remove('animate__slideInDown');
+  })
+  
+  e_2.classList.toggle('show');
+  e_2.focus({focusVisible: false});
+});
+
+e_2.addEventListener('blur', ()=> {
+  e_2.classList.add('animate__fadeOut');
+
+  e_2.addEventListener('animationend', ()=> {
+    e_2.classList.remove('animate__fadeOut');
+    e_2.classList.remove('show');
+    e_2.classList.add('hidden');
+  }, {once: true})
+});
+
+// event listener for chat-option btn and chat-option at small screens.
 const chatSettingBtn = document.getElementById('chat-setting-btn');
 const chatOptions = document.getElementById('chat-settings');
 
 if (chatOptions != null) {
+
   chatSettingBtn.addEventListener('click', ()=> {
     chatOptions.classList.toggle('hidden');
     chatOptions.classList.add('animate__slideInDown');
